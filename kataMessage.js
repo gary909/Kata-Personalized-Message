0b47dd211ee7adacb527d4c93846dbe8f70279a3
gary <gary.white@lodgit.com>
2021-11-02 13:01:57 +0100

    code working

--- kataMessage.js
+++ kataMessage.js
@@ -1,5 +1,10 @@
 function greet (name, owner) {
     // Add code here
+    if (name == owner) {
+        return "Hello boss"
+    } else {
+        return "Hello guest"
+    }
 }
 
 console.log(greet('Daniel', 'Daniel')); // Return "Hello boss"
