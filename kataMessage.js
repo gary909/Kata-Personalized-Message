c675f922d908f257084ac53be9cd26c3cc8d0a7e
gary <gary.white@lodgit.com>
2021-11-02 12:58:51 +0100

    code & html frame

---
+++ kataMessage.js
@@ -0,0 +1,6 @@
+function greet (name, owner) {
+    // Add code here
+}
+
+console.log(greet('Daniel', 'Daniel')); // Return "Hello boss"
+console.log(greet('Greg', 'Daniel')); // Return "Hello guest"
